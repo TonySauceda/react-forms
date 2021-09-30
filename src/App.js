43546c730adcf0c7636161d04e02011eab1091a9
TonySauceda <asauceda@boz.mx>
2021-09-30 13:51:26 -0500

    Controlled components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { useState } from 'react'
+
 const App = () => {
-	return <div>Temp</div>
+	const [value, setValue] = useState({ normal: '', texto: '' })
+
+	const handleChange = (e) => {
+		// Hambas formas tienen el mismo comportamiento
+
+		setValue({ ...value, [e.target.name]: e.target.value })
+		// setValue((state) => ({ ...value, [e.target.name]: e.target.value }))
+	}
+
+	console.log(value)
+
+	return (
+		<div>
+			{value < 5 ? <span>Logitud minima de 5</span> : null}
+			<input type="text" name="normal" value={value.normal} onChange={handleChange} />
+			<textarea name="texto" onChange={handleChange} value={value.texto} />
+		</div>
+	)
 }
 
 export default App
